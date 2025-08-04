bb5358693aedcc7f60d65a5ddeb273eb3f39e146
Vanessa <vanecordelins@gmail.com>
2025-08-04 12:58:41 -0300

    ajuste do cross-platform no wdio

--- configs/wdio.shared.conf.mjs
+++ configs/wdio.shared.conf.mjs
@@ -3,21 +3,33 @@ export const config = {
   specs: ['./features/**/*.feature'],
   exclude: [],
   maxInstances: 1,
-  capabilities: [{
-    platformName: 'Android',
-    'appium:deviceName': 'Android Emulator',
-    'appium:automationName': 'UiAutomator2',
-    'appium:app': 'apps\\android.wdio.native.app.v1.0.8.apk',
-    'appium:autoGrantPermissions': true
-  }],
   logLevel: 'info',
   reporters: ['spec'],
   framework: 'cucumber',
   cucumberOpts: {
-    require: ['./steps/**/*.js'],
-    timeout: 60000,
+    require: ['./features/step-definitions/*.js'],
+    timeout: 60000
   },
   services: ['appium'],
   port: 4723,
   path: '/wd/hub',
+
+  capabilities: [
+    process.env.PLATFORM === 'ios'
+      ? {
+          platformName: 'iOS',
+          'appium:platformVersion': '16.2',
+          'appium:deviceName': 'iPhone 14',
+          'appium:automationName': 'XCUITest',
+          'appium:app': process.cwd() + '/apps/wdiodemoapp.app',
+          'appium:autoAcceptAlerts': true
+        }
+      : {
+          platformName: 'Android',
+          'appium:deviceName': 'Android Emulator',
+          'appium:automationName': 'UiAutomator2',
+          'appium:app': process.cwd() + '/apps/android.wdio.native.app.v1.0.8.apk',
+          'appium:autoGrantPermissions': true
+        }
+  ]
 };
