26266e401c0f12dabe47a8cc6a45dc6fbd181d8a
Vanessa <vanecordelins@gmail.com>
2025-08-04 18:12:28 -0300

    ajustando o report allure local

--- configs/wdio.local.conf.mjs
+++ configs/wdio.local.conf.mjs
@@ -37,5 +37,23 @@ export const config = {
           'appium:app': path.join(rootPath, 'apps', 'android.wdio.native.app.v1.0.8.apk'),
           'appium:autoGrantPermissions': true
         }
-  ]
+  ],
+
+  reporters: [
+    ['allure', {
+      outputDir: 'allure-results',
+      disableWebdriverStepsReporting: true,
+      disableWebdriverScreenshotsReporting: false,
+    }]
+  ],
+
+  onComplete: async function () {
+    const { execSync } = await import('child_process').then(mod => mod);
+    try {
+      execSync('npx allure generate allure-results --clean -o allure-report', { stdio: 'inherit' });
+      console.log('✅ Allure report generated com sucesso!');
+    } catch (err) {
+      console.error('❌ Erro ao gerar o relatório Allure:', err);
+    }
+  }
 };
